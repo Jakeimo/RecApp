7ced0d058c99128fa04571149cfc5b0ef5d7bac4
Jake Spicer <Jakeimo@gmail.com>
2015-04-19 16:41:31 +1000

    Add function to get each each image from subfolder into array

--- app/src/main/java/com/example/jake/recapp/MainActivity.java
+++ app/src/main/java/com/example/jake/recapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.jake.recapp;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
@@ -14,13 +15,21 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //currently hardcoded, should be dynamically generated later on
-        setFolder("General");
-        setSubFolder("Fruit");
+        //currently hardcoded, should be dynamically generated later on e.g. user chooses folder and subfolder
+        //Tested for different arrays
+        setFolder("general");
+        setSubFolder("fruit");
 
         String breadcrumbText = getFolder() + " > " + getSubFolder();
         TextView breadcrumb = (TextView)findViewById(R.id.breadcrumb);
         breadcrumb.setText(breadcrumbText);
+
+        //Get all image names from string array in strings.xml, and put them in a String Array []
+        String stringArray = getFolder() + "_" + getSubFolder() + "_array";
+        Resources res = getResources();
+        int id = res.getIdentifier(stringArray, "array", getPackageName());
+        String[] objects = res.getStringArray(id);
+
     }
 
 
